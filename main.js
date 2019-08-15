5fc80f7c9e59759302042690bcd5337ac2b8f191
danish287 <dae19@duke.edu>
2019-08-14 21:43:09 -0600

    built first page of website

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-// Animations init
-new WOW().init();
+<input type="button" class="btn btn-info" value="Input Button" onclick=" relocate_home()">
+
+<script>
+function relocate_home()
+{
+     location.href = "www.yoursite.com";
+} 
+</script>
