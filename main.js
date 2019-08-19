af5487df0dc8ba42080db94e83887014c3d0c5df
danish287 <dae19@duke.edu>
2019-08-18 20:31:34 -0600

    formatted

--- main.js
+++ main.js
@@ -26,7 +26,6 @@ $(function() {
     var documentEl = $(document),
         fadeElem = $('.fade-out');
     var temp = $('.temp').offset().top;
-
     /*once we scroll webpage lets make changes on elements in webpage*/
     documentEl.on('scroll', function() {
         /*get position of how far we have scrolled from the top in pixels*/
@@ -36,22 +35,13 @@ $(function() {
             /* $this is the element we sare currenttly iterating through*/
             /*retireve and save every position on a variable so that 
              elements start to fadeout when  they reach the top of the page*/
-            
             var $this = $(this),
                 elemOffsetTop = $this.offset().top;
             /*we ahve to check if fadescroll element has reached top of page*/
-            if ((currScrollPos+temp)  > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/400);
-/*          also works but doesnt put elements back
-            if (currScrollPos  > elemOffsetTop) $this.css('opacity', 1 - $(window).scrollTop() / 300);*/
-        }); 
+            if ((currScrollPos + temp) > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos - elemOffsetTop) / 400);
+            /*          also works but doesnt put elements back
+                        if (currScrollPos  > elemOffsetTop) $this.css('opacity', 1 - $(window).scrollTop() / 300);*/
+        });
     });
-    
+
 });
-
-
-
-/*$(document).ready(function(){
-  $(".intro-p").scroll(function(){
-    $("span").text( x+= 1);
-  });
-});*/
